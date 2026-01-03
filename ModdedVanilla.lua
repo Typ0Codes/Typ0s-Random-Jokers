d560a3434b8d62f2880c8265ddd7ed2de3630b2b
Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
2026-01-03 01:35:37 -0700

    fixed

--- ModdedVanilla.lua
+++ ModdedVanilla.lua
@@ -40,7 +40,7 @@ SMODS.Atlas {
 SMODS.Joker{
     key = "black_spot",
     rarity = 3,
-    cost = 6,
+    cost = 8,
     atlas = "ModdedVanilla",
     pos = { x = 0, y = 0 },
 	loc_txt = {
@@ -84,10 +84,10 @@ SMODS.Joker {
 			"{C:green}(ex: 1 in 3 -> 1 in 6){}"
         }
     },
-    unlocked = false,
     blueprint_compat = false,
+    discovered = true,
     rarity = 2,
-    cost = 4,
+    cost = 5,
 	atlas = "ModdedVanilla",
     pos = { x = 1, y = 0 },
     calculate = function(self, card, context)
@@ -136,7 +136,7 @@ SMODS.Seal {
 SMODS.Joker{
     key = "Boykisser",
     rarity = 4,
-    cost = 5,
+    cost = 20,
     atlas = "ModdedVanilla",
     pos = { x = 0, y = 1 },
 	soul_pos = { x = 4, y = 1 },
@@ -168,6 +168,117 @@ SMODS.Joker{
         end
     end
 }
+
+SMODS.Joker {
+	-- How the code refers to the joker.
+	key = 'cooljimbo',
+	-- loc_text is the actual name and description that show in-game for the card.
+	loc_txt = {
+		name = 'Cool Jimbo',
+		text = {
+			--[[
+			The #1# is a variable that's stored in config, and is put into loc_vars.
+			The {C:} is a color modifier, and uses the color "mult" for the "+#1# " part, and then the empty {} is to reset all formatting, so that Mult remains uncolored.
+				There's {X:}, which sets the background, usually used for XMult.
+				There's {s:}, which is scale, and multiplies the text size by the value, like 0.8
+				There's one more, {V:1}, but is more advanced, and is used in Castle and Ancient Jokers. It allows for a variable to dynamically change the color. You can find an example in the Castle joker if needed.
+				Multiple variables can be used in one space, as long as you separate them with a comma. {C:attention, X:chips, s:1.3} would be the yellow attention color, with a blue chips-colored background,, and 1.3 times the scale of other text.
+				You can find the vanilla joker descriptions and names as well as several other things in the localization files.
+				]]
+			"{C:mult}+#1# {} Mult"
+		}
+	},
+	--[[
+		Config sets all the variables for your card, you want to put all numbers here.
+		This is really useful for scaling numbers, but should be done with static numbers -
+		If you want to change the static value, you'd only change this number, instead
+		of going through all your code to change each instance individually.
+		]]
+	config = { extra = { mult = 8 } },
+	-- loc_vars gives your loc_text variables to work with, in the format of #n#, n being the variable in order.
+	-- #1# is the first variable in vars, #2# the second, #3# the third, and so on.
+	-- It's also where you'd add to the info_queue, which is where things like the negative tooltip are.
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.mult } }
+	end,
+	-- Sets rarity. 1 common, 2 uncommon, 3 rare, 4 legendary.
+	rarity = 1,
+	-- Which atlas key to pull from.
+	atlas = 'ModdedVanilla',
+	-- This card's position on the atlas, starting at {x=0,y=0} for the very top left.
+	pos = { x = 2, y = 0 },
+	-- Cost of card in shop.
+	cost = 2,
+	-- The functioning part of the joker, looks at context to decide what step of scoring the game is on, and then gives a 'return' value if something activates.
+	calculate = function(self, card, context)
+		-- Tests if context.joker_main == true.
+		-- joker_main is a SMODS specific thing, and is where the effects of jokers that just give +stuff in the joker area area triggered, like Joker giving +Mult, Cavendish giving XMult, and Bull giving +Chips.
+		if context.joker_main then
+			-- Tells the joker what to do. In this case, it pulls the value of mult from the config, and tells the joker to use that variable as the "mult_mod".
+			return {
+				mult_mod = card.ability.extra.mult,
+				-- This is a localize function. Localize looks through the localization files, and translates it. It ensures your mod is able to be translated. I've left it out in most cases for clarity reasons, but this one is required, because it has a variable.
+				-- This specifically looks in the localization table for the 'variable' category, specifically under 'v_dictionary' in 'localization/en-us.lua', and searches that table for 'a_mult', which is short for add mult.
+				-- In the localization file, a_mult = "+#1#". Like with loc_vars, the vars in this message variable replace the #1#.
+				message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+				-- Without this, the mult will stil be added, but it'll just show as a blank red square that doesn't have any text.
+			}
+		end
+	end
+}
+
+
+
+   SMODS.Joker {
+    key = 'coolerjimbo',
+    loc_txt = {
+        name = 'Cooler Jimbo',
+        text = {
+            "{C:mult}+#1# {} Mult.",
+            "If you have {C:attention}Cool Jimbo{}",
+            "on the left of this joker,",
+            "{C:mult}X4{} Mult instead"
+        }
+    },
+    config = { extra = { mult = 8 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.mult } }
+    end,
+    rarity = 3,
+    atlas = 'ModdedVanilla',
+    pos = { x = 3, y = 0 },
+    cost = 2,
+    calculate = function(self, card, context)
+        if not context.joker_main then return end
+        if card.area ~= G.jokers then return end
+
+        local index
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == card then
+                index = i
+                break
+            end
+        end
+        if not index then return end
+
+        local left_joker = index > 1 and G.jokers.cards[index - 1] or nil
+        local left_key = left_joker and left_joker.original_key
+
+        if left_key == 'Typ0_cooljimbo' then
+            return {
+                Xmult_mod = 4,
+                message = localize { type = 'variable', key = 'a_xmult', vars = { 4 } }
+            }
+        else
+            return {
+                mult_mod = card.ability.extra.mult,
+                message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+            }
+        end
+    end
+}
+
+
 
 
 
